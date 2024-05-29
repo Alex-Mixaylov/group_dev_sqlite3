eede42792be9b319b43fdb5add4a2d8ae3721c75
Alex <guardamar81@gmail.com>
2024-05-29 20:25:58 +0400

    execute

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
+# import sqlite3
+#
+# conn = sqlite3.connect('example.db')
+# cur = conn.cursor()
+#
+# cur.execute('''
+#     CREATE TABLE IF NOT EXISTS users
+#     (id INTEGER PRIMARY KEY AUTOINCREMENT,
+#     name TEXT NOT NULL,
+#     age INTEGER NOT NULL)
+#         ''')
+# conn.commit()
+#
+# cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("John", 25))
+# cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Jane", 20))
+# conn.commit()
+#
+# conn.close()
+
 import sqlite3
 
 conn = sqlite3.connect('example.db')
 cur = conn.cursor()
 
-cur.execute('''
-    CREATE TABLE IF NOT EXISTS users
-    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL)
-        ''')
+cur.execute("SELECT * FROM users")
+rows = cur.fetchall()
+for row in rows:
+    print(row)
+
 conn.commit()
 
-cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("John", 25))
-cur.execute("INSERT INTO users (name, age) VALUES (?, ?)", ("Jane", 20))
-conn.commit()
 
-conn.close()
